ed422382c190265cf5276191a5b8a43c6bf743e8
Prima20 <primakurnia71@gmail.com>
2018-12-06 18:59:53 +0700

    Add template menu
    Minus background menu asset

--- BtOAv/Assets/Scripts/Main Menu/ButtonController.cs
+++ BtOAv/Assets/Scripts/Main Menu/ButtonController.cs
@@ -5,14 +5,47 @@ using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour {
 
+    public string sceneName;
+    public float zoomButtonSize;
+    public AudioSource slideAudio;
 
-	public void MoveToScene(string sceneName)
+    private Vector3 originalButtonSize;
+
+    public void MoveToScene()
     {
-        //SceneManager.LoadScene(sceneName);
 
-        if (gameObject.GetComponent<AudioSource>() == null)
+        if (gameObject.GetComponent<AudioSource>() != null)
         {
-            Debug.Log("Audio doesn't exist");
+            gameObject.GetComponent<AudioSource>().Play();
+            StartCoroutine(PlayAudio());
         }
+    }
+
+    public void zoomInButton()
+    {
+        originalButtonSize = transform.localScale;
+        transform.localScale = new Vector3(zoomButtonSize, zoomButtonSize, zoomButtonSize);
+        if(slideAudio != null)
+        {
+            slideAudio.Play();
+        }
+    }
+
+    public void zoomOutButton()
+    {
+        transform.localScale = originalButtonSize;
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+    IEnumerator PlayAudio()
+    {
+        yield return new WaitForSeconds(gameObject.GetComponent<AudioSource>().clip.length);
+        SceneManager.LoadScene(sceneName);
     }
 }
